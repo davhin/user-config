5495f77567af56647cf34b30a754589956ae59a2
David <mail@davidhinrichs.de>
2019-11-24 19:38:33 +0100

    move configs script to hub-images repo

--- copy_configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/idalab-de/user-config
-cd user-config
-cp .condarc /home/$NB_USER
-cp jupyter_notebook_config.py /home/$NB_USER/.jupyter
-cp overrides.json /opt/conda/share/jupyter/lab/settings
-export SHELL=/usr/bin/zsh
-conda init zsh
